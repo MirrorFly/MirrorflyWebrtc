330fdc042203b86946c15759e2df242ef6be2dd9
Gowtham R A <gowtham.a@contus.in>
2023-11-15 17:44:49 +0530

    Project changes

---
+++ MirrorFlyWebrtc/MirrorFlyWebrtc/MirrorFlyWebrtc.h
@@ -0,0 +1,18 @@
+//
+//  MirrorFlyWebrtc.h
+//  MirrorFlyWebrtc
+//
+//  Created by Gowtham on 15/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MirrorFlyWebrtc.
+FOUNDATION_EXPORT double MirrorFlyWebrtcVersionNumber;
+
+//! Project version string for MirrorFlyWebrtc.
+FOUNDATION_EXPORT const unsigned char MirrorFlyWebrtcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MirrorFlyWebrtc/PublicHeader.h>
+
+
